c34dfe7a7d4266c7f779d994c794db01647c0f8b
Noel <noelmugisha332@gmail.com>
2025-10-19 16:23:16 +0200

    Added interceptors

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    const contextValue = {
+        user,
+        setUser
+    };
+
+    return (
+        <AppContext.Provider value={contextValue}>
+            {children}
+        </AppContext.Provider>
+    )
+}
---
+++ frontend/src/utils/AxiosConfig.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.BACKEND_URL;
+
+const AxiosConfig = axios.create({
+    baseURL: baseURL,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+const excludeEndpoints = ["/auth/login", "/auth/register", "/auth/activate", "health"];
+
+// request interceptor
+AxiosConfig.interceptors.request.use((config) => {
+    const shouldSkipToken = excludeEndpoints.some((endpoint) => config.url?.includes(endpoint));
+
+    if (!shouldSkipToken) {
+        const accessToken = localStorage.getItem("token");
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+// response interceptor
+AxiosConfig.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (error.response && error.response.status === 401) {
+        window.location.href = "/login";
+    } else if (error.response && error.response.status === 500) {
+        console.error("Server Error:", error.response.data);
+    } else if (error.code === 'ECONNABORTED') {
+        console.error("Request timed out:", error.message);
+    }
+    return Promise.reject(error);
+});
+
+export default AxiosConfig;
