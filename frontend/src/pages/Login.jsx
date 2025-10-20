067af6cbd146f10378c2fb75a3d9f3f2f1e468eb
Noel <noelmugisha332@gmail.com>
2025-10-20 18:45:32 +0200

    Fully functional login logic

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,70 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
 import Input from "../components/Input";
+import axiosConfig from "../utils/axiosConfig";
+import { API_ENDPOINTS } from "../utils/apiEndpoints";
+import toast from "react-hot-toast";
+import AppContext from "../context/AppContext";
+import { LoaderCircle } from "lucide-react"; 
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
-
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const {setUser} = useContext(AppContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+
+    if (!email.trim()) {
+      setError("Please enter your email address");
+      setIsLoading(false); 
+      return;
+    }
+    if (!password.trim()) {
+      setError("Please enter your password");
+      setIsLoading(false); 
+      return;
+    }
+
+    // login API call
+    try {
+      const response = await axiosConfig.post(API_ENDPOINTS.LOGIN, {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        const { accessToken, user } = response.data; // Destructure accessToken and user
+
+        localStorage.setItem("token", accessToken);
+        setUser(user); 
+        toast.success("Login successful! Redirecting to dashboard...");
+        navigate("/dashboard"); 
+      }
+    } catch (err) {
+      console.log("Login Error:", err);
+      if (err.response && err.response.data) {
+        if (err.response.status === 401) {
+            setError("Invalid email or password.");
+        }
+        else if (err.response.data.message) {
+            setError(err.response.data.message);
+        } else {
+            setError("Authentication failed.");
+        }
+      } else {
+        setError("An unexpected network error occurred.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="h-screen w-full flex items-center justify-center overflow-hidden">
@@ -26,7 +82,7 @@ const Login = () => {
             Please enter your details to login to your account.
           </p>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit} className="space-y-4">
             <Input
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -51,12 +107,18 @@ const Login = () => {
 
             <div className="flex justify-center">
               {" "}
-              {/* New wrapper for centering */}
-              <button
-                className="btn-primary py-3 text-lg font-medium max-w-sm w-full"
+              <button disabled={isLoading} 
+                className={`btn-primary py-3 text-lg font-medium max-w-sm w-full flex items-center justify-center gap-2 ${isLoading ? 'opacity-70 cursor-not-allowed' : ''}`}
                 type="submit"
               >
-                LOGIN
+                {isLoading ? (
+                  <>
+                  <LoaderCircle className="animate-spin w-5 h-5"/>
+                  Logging In...
+                  </>
+                ): (
+                  "LOGIN"
+                )}
               </button>
             </div>
             <p className="text-sm text-slate-800 text-center mt-6">
